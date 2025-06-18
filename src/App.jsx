ae36ee7cd696aff34b0f82de5004fc44194faa0d
ka1sser <serenokaisser.dev@gmail.com>
2025-06-18 18:20:58 +0800

    Food Menu UI using useEffect to fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Todo from './components/Todo'
+import Meals from './components/Meals'
 
 const App = () => {
   return (
     <div>
-      <Todo/>
+      <Meals/>
     </div>
   )
 }
