2458101a4383035f665992d49b4d9c4591afe988
ka1sser <serenokaisser.dev@gmail.com>
2025-06-18 18:53:34 +0800

    Simple calculator app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Meals from './components/Meals'
+import Calculator from './components/Calculator';
 
 const App = () => {
   return (
     <div>
-      <Meals/>
+      <Calculator/>
     </div>
   )
 }
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "../style.css";
+
+const Calculator = () => {
+    const [inputvalue, setInputValue] = useState("");
+    
+    const display = value => {
+        setInputValue(inputvalue + value)
+    }
+    const clear = () => setInputValue("")
+    const calculate = () => {
+        setInputValue(eval(inputvalue))
+    }
+
+  return (
+    <form className="calculator" name="calc">
+          <input type="text" className="value" value={inputvalue} />
+          <span className="num clear" onClick={() => clear()}>
+              c
+          </span>
+          <span onClick={() => display('/')}>/</span>
+          <span onClick={() => display('*')}>*</span>
+          <span onClick={() => display('7')}>7</span>
+          <span onClick={() => display('8')}>8</span>
+          <span onClick={() => display('9')}>9</span>
+          <span onClick={() => display('-')}>-</span>
+          <span onClick={() => display('4')}>4</span>
+          <span onClick={() => display('5')}>5</span>
+          <span onClick={() => display('6')}>6</span>
+          <span className="plus" onClick={() => display("+")}>
+              +
+          </span>
+          <span onClick={() => display('1')}>1</span>
+          <span onClick={() => display('2')}>2</span>
+          <span onClick={() => display('3')}>3</span>
+          <span onClick={() => display('0')}>0</span>
+          <span onClick={() => display('00')}>00</span>
+          <span onClick={() => display('.')}>.</span>
+          <span className="num equal" onClick={() => calculate()}>
+              =
+          </span>
+    </form>
+  );
+};
+
+export default Calculator;
